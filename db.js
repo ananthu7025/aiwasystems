1053d1414695fef8f5db1e5192af317fca6a85b6
Ananthapadmanabhan <ananthapadmanabhan@igdst.com>
2023-09-22 10:29:11 +0530

    db

--- db.js
+++ db.js
@@ -1,23 +1,11 @@
 const mysql = require('mysql2');
+const { dbConfig } = require('./config');
 
-const pool = mysql.createPool({
-    host: "localhost", 
-    user: "root", 
-    password: "",
-    database: "aiwasystems",
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    port: 3306 // Default MySQL port
+const db = mysql.createConnection(dbConfig);
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL database');
 });
 
-pool.getConnection((err, conn) => {
-    if (err) console.error(err);
-    console.log("Connected successfully");
-    if (conn) {
-        conn.release(); // Release the connection back to the pool.
-    }
-});
-
-// Export the promise-based pool
-module.exports = pool.promise();
+module.exports = db;
