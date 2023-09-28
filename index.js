be90e62e9fca447843c783800b24b7e0d91ce54f
Ananthapadmanabhan <ananthapadmanabhan@igdst.com>
2023-09-28 12:50:19 +0530

    port added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //index.js
 const express = require('express');
 const cors = require('cors');
-const { port } = require('./config');
+// const { port } = require('./config');
 const mainCategoriesRoutes = require('./mainCategories');
 const subCategoriesRoutes = require('./subCategories');
 const productsRoutes = require('./products');
@@ -25,7 +25,7 @@ app.use('/main_categories', mainCategoriesRoutes);
 app.use('/sub_categories', subCategoriesRoutes);
 app.use('/products', productsRoutes);
 app.use('/cart', cartRoutes);
-
+const port =3030
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
