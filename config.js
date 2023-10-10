48b7a7af2c97c9bc771f02696f62ebb5d04b0663
ananthu7025 <93914698+ananthu7025@users.noreply.github.com>
2023-10-10 16:27:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 //config.js
 module.exports = {
     dbConfig: {
-      host: 'localhost',
-      user: 'root',
-      password: 'root',
-      database: 'aiwasystems'
+      host: 'b9ezqg3en2qihik3q3uq-mysql.services.clever-cloud.com',
+      user: 'ukvldoddb6zucjt1',
+      password: 'ukvldoddb6zucjt1',
+      database: 'b9ezqg3en2qihik3q3uq'
     },
     // port: 4000
   };
