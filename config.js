bf4be49f526d76666b2eed2beeeff0f569544dc6
ananthu7025 <93914698+ananthu7025@users.noreply.github.com>
2023-10-10 16:37:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     dbConfig: {
       host: 'b9ezqg3en2qihik3q3uq-mysql.services.clever-cloud.com',
       user: 'ukvldoddb6zucjt1',
-      password: '',
+      password: 's1YNRrKcaRyPCxGHDD1n',
       database: 'b9ezqg3en2qihik3q3uq'
     },
     // port: 4000
